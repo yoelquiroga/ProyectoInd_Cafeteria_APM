a768c6b9a85e30864d61d023785c4c5442d547ae
yoelquiroga <yoelquirogav12@gmail.com>
2025-09-28 04:34:06 -0500

    Implementacion de splash y activity bienvenida

---
+++ app/src/main/java/com/example/proyectocafeteria/BienvenidaActivity.kt
@@ -0,0 +1,37 @@
+package com.example.proyectocafeteria
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+
+
+class BienvenidaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bienvenida)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+            // Referencia al botón
+            val btnEmpezar = findViewById<Button>(R.id.btnEmpezar)
+
+            // Acción: abrir AccesoActivity
+            btnEmpezar.setOnClickListener {
+                val intent = Intent(this, AccesoActivity::class.java)
+                startActivity(intent)
+                finish() // opcional: cierra la BienvenidaActivity para que no regrese al splash
+
+        }
+    }
+}
--- app/src/main/java/com/example/proyectocafeteria/SplashActivity.kt
+++ app/src/main/java/com/example/proyectocafeteria/SplashActivity.kt
@@ -1,20 +1,44 @@
 package com.example.proyectocafeteria
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Instalar el splash screen nativo
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+
+        // Ajustar padding para las barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Animación del logo
+        val logo = findViewById<ImageView>(R.id.icSplash)
+        val slideIn = AnimationUtils.loadAnimation(this, R.anim.deslizar_in)
+        logo.startAnimation(slideIn)
+
+        // Espera 2 segundos y abre la pantalla de bienvenida
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, BienvenidaActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
