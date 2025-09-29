1cb625505b77d2184819a549480b6dc05e16f03a
yoelquiroga <yoelquirogav12@gmail.com>
2025-09-29 04:41:43 -0500

    Implementacion del activity_acceso

--- app/src/main/java/com/example/proyectocafeteria/RegistrarseActivity.kt
+++ app/src/main/java/com/example/proyectocafeteria/RegistrarseActivity.kt
@@ -1,34 +1,20 @@
 package com.example.proyectocafeteria
 
-
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AccesoActivity : AppCompatActivity() {
+class RegistrarseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_acceso)
-
-
+        setContentView(R.layout.activity_registrarse)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        findViewById<View>(R.id.tvRegistrarse).setOnClickListener {
-            val intent = Intent(this, RegistrarseActivity::class.java)
-            startActivity(intent)
-        }
-
-
-
-
     }
 }
