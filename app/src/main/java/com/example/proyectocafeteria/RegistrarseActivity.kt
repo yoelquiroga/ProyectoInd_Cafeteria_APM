cc5c6a46e0a8a33271020bb202f12f20086803e3
yoelquiroga <yoelquirogav12@gmail.com>
2025-09-29 06:01:21 -0500

    Cambios en el activity acceso + creacion de activity registrase

--- app/src/main/java/com/example/proyectocafeteria/RegistrarseActivity.kt
+++ app/src/main/java/com/example/proyectocafeteria/RegistrarseActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectocafeteria
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,14 @@ class RegistrarseActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+        findViewById<View>(R.id.tvIniciarSesion).setOnClickListener {
+            val intent = Intent(this, AccesoActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
